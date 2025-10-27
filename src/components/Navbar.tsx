8d500df12d4dd562599b2694ffd9f5125f51ba69
Naveen Kumar P <naveenkumar11202006@gmail.com>
2025-10-27 19:59:53 +0530

    logooo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -34,7 +34,9 @@ const Navbar = () => {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex justify-between items-center">
           <Link to="/" className="flex items-center gap-2">
-            <Code size={28} className="text-blue-600" />
+             <img src="/lo.png"
+              alt="Deep Ware Studio Logo"
+              className="h-10 w-auto object-contain"/>
             <span className="font-bold text-xl text-gray-900">Deep Ware Studio</span>
           </Link>
 
