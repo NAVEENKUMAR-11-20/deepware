b58d793938211499e2b6c42b1a195bf4b41bee5b
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-18 14:19:25 +0530

    cf

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,17 +68,17 @@ const Navbar = () => {
           className="md:hidden absolute top-full left-0 right-0 p-4 bg-white text-black shadow-xl"
         >
           <div className="flex flex-col space-y-4">
-            <Link to="/#home" className="px-4 py-2 font-medium text-gray-800 hover:text-blue-600">
+            <a href="#home" className="px-4 py-2 font-medium text-gray-800 hover:text-blue-600">
               Home
-            </Link>
+            </a>
 
-            <Link to="#services" className="px-4 py-2 font-medium text-gray-800 hover:text-blue-600">
+            <a href="#services" className="px-4 py-2 font-medium text-gray-800 hover:text-blue-600">
               Services
-            </Link>
+            </a>
 
-            <Link to="/#testimonials" className="px-4 py-2 font-medium text-gray-800 hover:text-blue-600">
+            <a href="#testimonials" className="px-4 py-2 font-medium text-gray-800 hover:text-blue-600">
               Testimonials
-            </Link>
+            </a>
 
             <Link
               to="/register"
