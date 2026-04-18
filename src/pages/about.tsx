40fd0f5b17cfb49f232f4b1d09244a1b1b9ae625
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-18 19:48:28 +0530

    ref

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,7 +11,7 @@ const team = [
   },
   {
     name: "Dhinesh S",
-    role: "UI / UX Designer",
+    role: "Founder & Full Stack Developer & UI / UX Designer",
     image: "/team2.jpg",
     description:
       "Creative designer crafting beautiful, user-friendly interfaces and engaging user experiences.",
@@ -66,7 +66,6 @@ const About = () => {
               {/* image */}
               <img
                 src={member.image}
-                alt={member.name}
                 className="w-32 h-32 mx-auto rounded-full object-cover border-4 border-blue-400 mb-4"
               />
 
