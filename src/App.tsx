51b5af10adf441297721df06ebd81234e390e07b
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-18 19:30:39 +0530

    ss

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/about" element={<About />} />
+        <Route path="about" element={<About />} />
         <Route path="register" element={<RegistrationPage />} />
       </Route>
     </Routes>
