ba543198110cd0e917c5fb0295c96366b2325d9b
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-18 12:14:26 +0530

    fr

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
       }`}
     >
       <div className="container mx-auto px-4 md:px-6">
-        <div className="flex justify-between items-center h-[120px]">
+        <div className="flex justify-between items-center h-[80px]">
           <Link to="/" className="flex items-center gap-2">
              <img src="/logo.png"
               alt="DenveX Studio Logo"
