bf470a2d9af7ca5f7fd79eef8787dedab77b36fe
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-25 14:07:21 +0530

    ry

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
 const Layout = () => {
+  const location = useLocation();
+  const hideNavbarAndFooter = location.pathname === '/terms-and-conditions';
+
   return (
     <div className="flex flex-col min-h-screen bg-slate-50">
-      <Navbar />
+      {!hideNavbarAndFooter && <Navbar />}
       <main className="flex-grow">
         <Outlet />
       </main>
-      <Footer />
+      {!hideNavbarAndFooter && <Footer />}
     </div>
   );
 };
