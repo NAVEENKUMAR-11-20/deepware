538ab1a96ea5c84d9ffdeb181760b99429a1b131
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-04-25 13:04:37 +0530

    de

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -134,7 +134,7 @@ const Footer = () => {
         <div className="border-t border-gray-800 mt-12 pt-8 text-center text-gray-500 text-sm">
           <div className="flex flex-col items-center justify-center gap-3 sm:flex-row sm:gap-4">
             <img
-              src="/msme-logo.png"
+              src="https://www.bing.com/ck/a?!&&p=4b9a401faee8a776f4cbb98edda57306ceb51cb0b46e30df25b6f55d670fdecfJmltdHM9MTc3NzA3NTIwMA&ptn=3&ver=2&hsh=4&fclid=0fc6cc50-50b4-652d-0348-da2651d6643c&u=a1L2ltYWdlcy9zZWFyY2g_cT1tc21lK2xvZ28maWQ9QjZEMzdBNEQ1MkQ2QjM5Q0JEQjU5QkM3QzM4N0JCNjNCMzBCOTM5OSZGT1JNPUlRRlJCQQ"
               alt="MSME Registered Logo"
               className="h-10 w-auto object-contain"
             />
