8b05aff10826fd1645b4a7ded75928531ad84973
Naveen Kumar P <naveenkumar11202006@gmail.com>
2026-05-02 22:18:58 +0530

    ssd

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -17,7 +17,7 @@ const SEO = ({
   keywords = "web design platform, web development services, modern website design agency, UI/UX design company, custom web solutions, DenveX",
   ogTitle,
   ogDescription,
-  ogImage = "/Untitled design (12).png",
+  ogImage = "/favicon-2.png",
   canonical,
   schema
 }: SEOProps) => {
